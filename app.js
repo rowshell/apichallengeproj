7a64dc0d2c5a2bc5964f833617a492116ad08053
rowshell <rowshell@users.noreply.github.com>
2015-08-13 18:50:53 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
-/*Makes page more interactive*/
+/* Makes page more interactive */
 
+var main = function() { 
+  $('.icon-menu');
+}; 
 
+$(document).ready(main); /* runs the main function after HTML doc has fully loaded */
