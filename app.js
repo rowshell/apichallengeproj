28f1afa82303205968f0273c971f3b21145b432a
rowshell <rowshell@users.noreply.github.com>
2015-08-13 19:03:22 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 /* Makes page more interactive */
 
 var main = function() { 
-  $('.icon-menu').click();
+  $('.icon-menu').click(function() {
+    $('.menu').animate({ 
+      left:'0px'
+    }, 200);
+    
+    $('body').animate( {
+      left: '285px'
+    }, 200);
+  });
   
 }; 
 
