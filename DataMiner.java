ae1a21870579549955f33d7752fe6a07ec9e4629
jssmith1331 <jssmith1331@gmail.com>
2015-08-16 21:51:45 -0700

    Version 4

--- DataMiner.java
+++ DataMiner.java
@@ -15,6 +15,10 @@ Version 2.1 - Added package bmb (Black Market Buddy)
 Version 2.2 - Changed data type to enums for better usability. Items are now in Items.java
 Version 3.0 - Added methods to get the champion and item purchased by a summoner
             - Added method to get Kills/Deaths/Assists and calculate KDA
+Version 4.0 - Added method to get win rate for a champ or item
+            - Added class statHolder and fields itemList and champList for data storage
+            - Updated getChamp and getItem to store data on win/loss
+            - Began work on main
 
 */
 package bmb;
@@ -30,6 +34,8 @@ public class DataMiner{
 
   private static String matchAPI = "https://na.api.pvp.net/api/lol/na/v2.2/match/";
   private static URL url;
+  private static ArrayList<statHolder> itemList = new ArrayList<statHolder>();
+  private static ArrayList<statHolder> champList = new ArrayList<statHolder>();
 
   /////////SECURE THIS LATER!////////
   private static String key = "?api_key=cd294b04-4505-4d18-89b8-6d1f0298d920";
@@ -38,19 +44,78 @@ public class DataMiner{
 
   public static void main(String[] args){
 
-    //////This is just for test/////////
-    int[] array = genArray("./Datasets/BILGEWATER/NA.json");
-
-
-    ////////////////////////////////////
-
+    if( args.length < 2 ){
+      printUsage();
+      return;
+    }
+    
+    String fileName = args[0];
+    ArrayList<int[]> matchIDs = new ArrayList<int[]>();
+    for(int i = 1; i < args.length; i++){
+      matchIDs.add(args[i].genArray());
+    }
+    
+    
+//TODO: Add some kind of functionality to write data to file
+//Include champion, item, and stat detail. Perhaps create a
+//JSONObject full of item objects. Each item object has a champ
+//object which has stats in it. Consider inner classes for item and champ
+//in order to temporarily store data before JSONObject creation
 
   }
 
   /**
-  * Method to calculate the KDA of a summoner in a match
+  * Method to write a JSONObject to a file
+  * @param filename the name of the json file
+  * @param obj the object to write
+  * */
+  private static void writeFile(String filename, JSONObject obj){
+    try{
+       FileWriter file = new FileWriter(filename);
+       file.write(obj.toJSONString());
+       file.flush();
+       file.close();
+    }
+    catch(IOException e){
+      e.printStackTrace();
+    }
+  }
+
+  /**
+  * Method to calculate the win rate of an item or champion
+  * @param isChamp whether or not the element is a champion
+  * @param id the id of the champion or item
+  * @return the win rate of the item of champion
+  * */
+  public static double winRate(boolean isChamp, int id){
+    int numGames = 0;
+    int numWins = 0;
+ 
+    //Access the appropriate ArrayList and sum wins and games
+    if(isChamp){
+      for(int i = 0; i < champList.size(); i++){
+        if(id == champList.get(i).champion){
+          numGames++;
+          numWins+=champList.get(i).win;
+        }
+      }
+    }
+    else{
+      for(int i = 0; i < itemList.size(); i++){
+        if(id == itemList.get(i).item){
+          numGames++;
+          numWins+=itemList.get(i).win;
+        }
+      }
+    }
+
+    return (double)(numWins/numGames);
+  }
+
+  /**
+  * Method to calculate the stats of a summoner in a match
   * @param summoner A JSONObject representing the summoner
-  * @return a double array containing kills, deaths, assists, and KDA
+  * @return a double array containing kills, deaths, assists, KDA
   * */
   private static double[] getKDA(JSONObject summoner){
     
@@ -67,11 +132,17 @@ public class DataMiner{
 
     double kda = (kills + assists)/deaths;
 
+    boolean win = (boolean)stats.get("winner");
+    int winner = 0;
+    if(win)
+      winner = 1; 
+
     double[] toReturn = new double[4];
     toReturn[0] = kills;
     toReturn[1] = actualDeaths;
     toReturn[2] = assists;
     toReturn[3] = kda;
+
 
     return toReturn;
   }
@@ -90,6 +161,14 @@ public class DataMiner{
     for(int i = 1; i < 7; i++){
       for( Items j : Items.values() ){
         if( (int)stats.get("item"+i) == j.id ){
+
+          //Determine if win or loss, and put data into itemList
+          boolean winner = (boolean)stats.get("winner");
+          int win = 0;
+          if(winner)
+            win = 1;
+
+          itemList.add( new statHolder(j.id, 0, win) );
           return j.name;
         }
       }
@@ -106,10 +185,19 @@ public class DataMiner{
   private static String getChamp(JSONObject summoner){
   
     int champId = (int)summoner.get("championId");
+    JSONObject stats = (JSONObject)summoner.get("stats");
   
     //Search through the Champions enum to find a match
     for(Champions c : Champions.values()){
       if(champId == c.id){
+
+        //Determine if win or loss, and put data into champList
+        boolean winner = (boolean)stats.get("winner");
+        int win = 0;
+        if(winner)
+          win = 1;
+
+        champList.add(new statHolder(0,c.id,win));
         return c.name;
       }
     }
@@ -219,6 +307,14 @@ public class DataMiner{
     return new int[0];
   }
 
+  public static void printUsage(){
+    System.out.println("Usage:");
+    System.out.println("Takes at least 2 args, the file to write to and the file(s) " +
+                        "containing Riot's dataset of match ids");
+    System.out.println("args[1] = fileName  //the name of the file to write to");
+    System.out.println("args[2:] = dataFile //the names of the files to read in");
+  }
+
      
 
 
@@ -236,8 +332,5 @@ public class DataMiner{
 
 
 
-
-
-
-
 }//End of class DataMiner
+
