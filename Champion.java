0fd744a2adf4a70a5367fadafffb65444040f595
jssmith1331 <jssmith1331@gmail.com>
2015-08-17 22:03:40 -0700

    Create Champion.java
    Sorry about the similar name

---
+++ Champion.java
@@ -0,0 +1,19 @@
+
+package bmb;
+
+
+public class Champion{
+  public String name = "";
+  public int id = 0;
+  public int games = 0;
+  public int wins = 0;
+  public double winRate = 0.0;
+  public double kda = 0.0;
+  
+  public Champion(){}
+
+  public Champion(String name){
+    this.name = name;
+  }
+  
+}
