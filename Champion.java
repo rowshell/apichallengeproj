cbd3b3960ac707f24b1284fa5ea3cbc669714d5d
jssmith1331 <jssmith1331@gmail.com>
2015-08-19 21:19:01 -0700

    Update Champion.java

--- Champion.java
+++ Champion.java
@@ -1,3 +1,9 @@
+/*
+Class that defines a Champion object.
+Champions are used in order to store data about
+a League champion
+@author Jeremy Seiji Smith
+*/
 
 package bmb;
 
@@ -9,6 +15,7 @@ public class Champion{
   public int wins = 0;
   public double winRate = 0.0;
   public double kda = 0.0;
+  public double pickRate = 0.0;
   
   public Champion(){}
 
