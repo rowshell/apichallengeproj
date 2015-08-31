be583c3443cba074c17d2e1fe3c89c51b1f44857
jssmith1331 <jssmith1331@gmail.com>
2015-08-30 17:20:32 -0700

    Create HomeWriter.js

---
+++ HomeWriter.js
@@ -0,0 +1,83 @@
+
+findBestItem();
+findWorstItem();
+
+function findBestItem(){
+  var currBest = "";
+  var win;
+  var kda;
+  var numChamps = 0;
+  var winDiff = 0;
+  var kdaDiff = 0;
+  var winDiffStore = 0;
+  var kdaDiffStore = 0;
+
+  for( var item in post ){
+    if( post.hasOwnProperty(item) ){
+
+      for( var champion in item ){
+ 
+        win = getDifference( 1, 0, 0, item, champion );
+        kda = getDifference( 0, 0, 1, item, champion );
+        if(win == "Unplayed" || kda == "Unplayed")
+          continue;
+        
+        winDiff += win;
+        kdaDiff += kda;
+        numChamps++;
+      }
+      if( winDiff + kdaDiff > currBest ){
+        currBest = item;
+        winDiffStore = winDiff;
+        kdaDiffStore = kdaDiff;
+      }
+    }
+  }
+  winDiffStore /= numChamps;
+  kdaDiffStore /= numChamps;
+  document.getElementById("increaseWinRate").innerHTML = Number(winDiffStore).toFixed(3);
+  document.getElementById("increaseKDA").innerHTML = Number(kdaDiffStore).toFixed(3);
+  document.getElementById("bestItemPic").src = ""; 
+  document.getElementById("bestItemPic").alt = currBest;
+  document.getElementById("bestItemPic").innerHTML = currBest;
+}
+
+function findWorstItem(){
+  var currWorst = "";
+  var win;
+  var kda;
+  var numChamps = 0;
+  var winDiff = 0;
+  var kdaDiff = 0;
+  var winDiffStore = 0;
+  var kdaDiffStore = 0;
+
+  for( var item in post ){
+    if( post.hasOwnProperty(item) ){
+
+      for( var champion in item ){
+ 
+        win = getDifference( 1, 0, 0, item, champion );
+        kda = getDifference( 0, 0, 1, item, champion );
+        if(win == "Unplayed" || kda == "Unplayed")
+          continue;
+
+        winDiff += win;
+        kdaDiff += kda;
+        numChamps++;
+      }
+      if( winDiff + kdaDiff < currWorst ){
+        currWorst = item;
+        winDiffStore = winDiff;
+        kdaDiffStore = kdaDiff;
+      }
+      winDiff = 0;
+      kdaDiff = 0;
+    }
+  }
+  document.getElementById("decreaseWinRate").innerHTML = Number(winDiffStore).toFixed(3);
+  document.getElementById("decreaseKDA").innerHTML = Number(kdaDiffStore).toFixed(3);
+  document.getElementById("worstItemPic").src = "";
+  document.getElementById("worstItemPic").alt = currWorst;
+  document.getElementById("worstItemPic").innerHTML = currWorst;
+}
