dd1398f40ccd60ab2c5bd6bb2c94f6922cda9a7b
jssmith1331 <jssmith1331@gmail.com>
2015-08-30 18:34:24 -0700

    Update HomeWriter.js

--- HomeWriter.js
+++ HomeWriter.js
@@ -19,6 +19,7 @@ function findBestItem(){
       for( var champion in item ){
         win = Number(getDifference( 1, 0, 0, item, champion ));
         kda = Number(getDifference( 0, 0, 1, item, champion ));
+        console.log(win);
         if(win == "Unplayed" || kda == "Unplayed")
           continue;
         
