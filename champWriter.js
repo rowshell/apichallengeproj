6acd46e5ce761a55e3003a4a2579a55e48e190c0
jssmith1331 <jssmith1331@gmail.com>
2015-08-30 23:07:58 -0700

    Update champWriter.js

--- champWriter.js
+++ champWriter.js
@@ -28,7 +28,7 @@ function write(){
   //Format the champ name in order to get the correct splash art
   var imgChampName = champName.replace(" ","");
   imgChampName = imgChampName.replace("'","");
-  var imgURL = "http://ddragon.leagueoflegends.com/cdn/img/champion.splash/" + imgChampName + "_0.jpg";
+  var imgURL = "http://ddragon.leagueoflegends.com/cdn/img/champion/splash/" + imgChampName + "_0.jpg";
 
   //Set each element in the page according to the champion's characteristics
   document.getElementById("splash").setAttribute("src",imgURL);
@@ -38,16 +38,59 @@ function write(){
   document.getElementById("champName").innerHTML = champName;
 
   //Determine the item that helped and hurt the champion the most
-  var bestItem = getBest(champName);
-  var worstItem = getWorst(champName);
+  var bestItem = findBest(champName);
+  if( bestItem == "" ){
+    console.log("no best item found");
+    return;
+  }
+  var worstItem = findWorst(champName);
+  
+  document.getElementById("helpItemPic").innerHTML = bestItem;
+  document.getElementById("helpItemPic").title = bestItem;
+  document.getElementById("helpLink").innerHTML = bestItem;
  
-  document.getElementById("helpPreWin").innerHTML = getChampionWinRate( bestItem, champName, 0 );
-  document.getElementById("helpPostWin").innerHTML = getChampionWinRate( bestItem, champName, 1 );
-  document.getElementById.("helpPreKDA").innerHTML = getChampionKDA( bestItem, champName, 0 );
-  document.getElementById("helpPostKDA").innerHTML = getChampionKDA( bestItem, champName, 1 );
+  document.getElementById("helpPreWin").innerHTML = Number(getChampionWinRate( bestItem, champName, 0 )).toFixed(3);
+  document.getElementById("helpPostWin").innerHTML = Number(getChampionWinRate( bestItem, champName, 1 )).toFixed(3);
+  document.getElementById("helpPreKDA").innerHTML = Number(getChampionKDA( bestItem, champName, 0 )).toFixed(3);
+  document.getElementById("helpPostKDA").innerHTML = Number(getChampionKDA( bestItem, champName, 1 )).toFixed(3);
 
-  document.getElementById("hurtPreWin").innerHTML = getChampionWinRate( worstItem, champName, 0 );
-  document.getElementById("hurtPostWin").innerHTML = getChampionWinRate( worstItem, champName, 1 );
-  document.getElementById("hurtPreKDA").innerHTML = getChampionKDA( worstItem, champName, 0 );
-  document.getElementById("hurtPostKDA").innerHTML = getChampionKDA( worstItem, champName, 1 );
+  document.getElementById("hurtItemPic").innerHTML = worstItem;
+  document.getElementById("hurtItemPic").title = worstItem;
+  document.getElementById("hurtLink").innerHTML = worstItem;
+
+  document.getElementById("hurtPreWin").innerHTML = Number(getChampionWinRate( worstItem, champName, 0 )).toFixed(3);
+  document.getElementById("hurtPostWin").innerHTML = Number(getChampionWinRate( worstItem, champName, 1 )).toFixed(3);
+  document.getElementById("hurtPreKDA").innerHTML = Number(getChampionKDA( worstItem, champName, 0 )).toFixed(3);
+  document.getElementById("hurtPostKDA").innerHTML = Number(getChampionKDA( worstItem, champName, 1 )).toFixed(3);
+  
+  var itemId = 0;
+  if(bestItem == "Dead Man's Plate") itemId = 3742;
+  else if(bestItem == "Flesheater") itemId = 3924;
+  else if(bestItem == "Globe of Trust") itemId = 3840;
+  else if(bestItem == "Martyr's Gambit") itemId = 3911;
+  else if(bestItem == "Mirage Blade") itemId = 3150;
+  else if(bestItem == "Netherstride Grimoire") itemId = 3431;
+  else if(bestItem == "Pox Arcana") itemId = 3434;
+  else if(bestItem == "Puppeteer") itemId = 3745;
+  else if(bestItem == "Rite of Ruin") itemId = 3430;
+  else if(bestItem == "Staff of Flowing Water") itemId = 3744;
+  else if(bestItem == "Trickster's Glass") itemId = 3829;
+  else if(bestItem == "Typhoon Claws") itemId = 3652;
+  
+  document.getElementById("helpItemPic").src = "http://ddragon.leagueoflegends.com/cdn/5.15.1/img/item/" + itemId + ".png";
+  
+  if(worstItem == "Dead Man's Plate") itemId = 3742;
+  else if(worstItem == "Flesheater") itemId = 3924;
+  else if(worstItem == "Globe of Trust") itemId = 3840;
+  else if(worstItem == "Martyr's Gambit") itemId = 3911;
+  else if(worstItem == "Mirage Blade") itemId = 3150;
+  else if(worstItem == "Netherstride Grimoire") itemId = 3431;
+  else if(worstItem == "Pox Arcana") itemId = 3434;
+  else if(worstItem == "Puppeteer") itemId = 3745;
+  else if(worstItem == "Rite of Ruin") itemId = 3430;
+  else if(worstItem == "Staff of Flowing Water") itemId = 3744;
+  else if(worstItem == "Trickster's Glass") itemId = 3829;
+  else if(worstItem == "Typhoon Claws") itemId = 3652;
+  
+  document.getElementById("hurtItemPic").src = "http://ddragon.leagueoflegends.com/cdn/5.15.1/img/item/" + itemId +".png";
 }
